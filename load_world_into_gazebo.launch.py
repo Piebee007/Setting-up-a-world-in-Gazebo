294d3d22a158307edd739400ee394a9b0ff15673
Piebee007 <74003575+Piebee007@users.noreply.github.com>
2022-08-31 10:43:18 +0100

    Update load_world_into_gazebo.launch.py

--- load_world_into_gazebo.launch.py
+++ load_world_into_gazebo.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
   pkg_share = FindPackageShare(package='two_wheeled_robot').find('two_wheeled_robot')
  
   # Set the path to the world file
-  world_file_name = 'ISAGroundFloorWorldV2.world'
+  world_file_name = 'CHANGE_ME.world'
   world_path = os.path.join(pkg_share, 'worlds', world_file_name)
    
   # Set the path to the SDF model files.
